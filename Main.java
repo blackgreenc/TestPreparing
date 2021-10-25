b2756caec9eeb135a88d8ec47d937a28bf29ff0f
black_green <88067206+blackgreenc@users.noreply.github.com>
2021-10-25 00:22:04 +0000

    class and object

--- Main.java
+++ Main.java
@@ -1,5 +1,34 @@
+//클래스 - 객체를 정의해 놓은 것, 객체의 설계도, 틀
+//객체  -  실제로 존재하는 것
+//인스터스 - 클래스로 부터 만들어진 객체
+//-> 객체를 사용하기 위해서(인스터스화 : 클래스로부터 객체를 만드는 과정)가 진행되어야 함
+
+//객체의 구성요소 - 속성과 기능
+//-멤버변수(필드) : 객체의 속성을 표현
+//-메서드 : 객체의 기능(동작)을 표현
+//->멤버변수(필드)와 메서드를 합쳐서 멤버라고 함
+
+/*
+class Car{
+    String name;
+    String color;
+    int seat;
+    float gas_mileage;
+
+    void carPrint(){
+        System.out.println("차량번호 : " + name);
+        System.out.println("색상 : " + color);
+        System.out.println("좌석 : " + seat +"인승");
+        System.out.println("연비 : " + gas_mileage);
+        System.out.println();
+    }
+}
+*/
+
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+
   }
 }
